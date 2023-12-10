e7e608f691950103a046a4809c4c1339edde4d9a
ymerza <151393703+ymerza@users.noreply.github.com>
2023-12-10 21:15:07 +0300

    Add files via upload

---
+++ hafta9 ödev.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+{
+    class Tank
+    {
+    public:
+       char tür;
+    private:
+       float litre;
+       {
+           return çevre%2.3:
+               cout<<çevre%2.3=çevre%6<<endl;
+       }
+    };
+    int main()
+
+}
